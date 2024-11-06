0e47a5d7ade74d50f447063f2ab6da2350abc4bc
kilpatrickap <kilpatrickap18@gmail.com>
2024-11-06 22:26:52 +0000

    vendor data in RFP.

--- BasicPricelist.py
+++ BasicPricelist.py
@@ -588,7 +588,7 @@ class BasicPricelist(QMainWindow):
         # Create the email body with the list of materials
         material_list = "\n".join(f"{i + 1}.  {material}" for i, material in enumerate(materials))
         email_body_text = (
-            f"Dear {vendor_name},\n\nI would like to Request For your Current Prices of the following materials:\n"
+            f"Dear {vendor_name},\n\nI would like to Request for your Current Prices of the following materials:\n"
             f"{material_list}\n\nBest regards,\n{user_name}\n\n{company_name}\n{user_position}\n{user_phone}"
         )
 
