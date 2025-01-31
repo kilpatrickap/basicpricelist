86892a57bbef79c9f2860e71a791f5c1444b087b
kilpatrickap18 <kilpatrickap18@gmail.com>
2025-01-31 11:27:38 +0000

    API authorized post integration done.

--- Materials_Manager.py
+++ Materials_Manager.py
@@ -1,3 +1,4 @@
+import requests
 import json
 import os
 import sys
@@ -310,7 +311,7 @@ class BasicPricelist(QMainWindow):
             return None
 
     def authorized_users_to_post_API(self):
-        authorized_users = ["kil", "pat"]
+        authorized_users = ["kil", "pat"]   #todo change user.
         return authorized_users
 
     def update_json(self):
@@ -339,8 +340,31 @@ class BasicPricelist(QMainWindow):
                 QMessageBox.critical(self, "Error", f"Failed to update JSON file: {str(e)}")
 
             print(f"{current_user} is authorized to post to API.")
+
+            # post json file to API
+            self.post_json_to_API()
+
         else:
             print(f"{current_user} is not authorized to post to API.")
+
+    def post_json_to_API(self):
+
+        # Read the materials-data.json file
+        parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "."))
+        json_path = os.path.join(parent_dir, "materials-data.json")
+
+        with open(json_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        # Post the data to the API root `/`
+        response = requests.post("https://mm-api-rz05.onrender.com", json=data)
+
+        # Check the response
+        if response.status_code == 200:
+            print("Data uploaded successfully.")
+        else:
+            print(f"Failed to upload data: {response.status_code} - {response.text}")
+
 
     def update_default_job_label(self, job_name):
         """Updates the current job label with the job name (db_file)."""
